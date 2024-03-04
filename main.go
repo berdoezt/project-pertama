41b55a070433468743f7a1f4c10b2038ffdab40d
Albert Mario <albert.rae@gojek.com>
2024-03-04 21:40:37 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"project-pertama/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	personController := controller.NewPersonController()
+
+	ginEngine := gin.Default()
+
+	ginEngine.GET("/person", personController.GetAll)
+	ginEngine.POST("/person", personController.Create)
+
+	err := ginEngine.Run("localhost:8082")
+	if err != nil {
+		panic(err)
+	}
+}
